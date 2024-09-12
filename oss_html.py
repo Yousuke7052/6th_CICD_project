1337fb21fa747dbf88ff1d70322c9052d047e0ac
littlesun <liohahaku@gmail.com>
2024-09-12 23:03:23 +0800

    update

--- oss_html.py
+++ oss_html.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-# 从环境变量中读取OSS相关信息
+# OSS相关环境变量
 OSS_ACCESS_KEY_ID = os.getenv('OSS_ACCESS_KEY_ID')
 OSS_SECRET_ACCESS_KEY = os.getenv('OSS_SECRET_ACCESS_KEY')
 OSS_ENDPOINT = os.getenv('OSS_ENDPOINT')
@@ -10,8 +10,6 @@ OSS_BUCKET_NAME = os.getenv('OSS_BUCKET_NAME')
 # GitHub仓库信息
 GITHUB_REPO_URL = os.getenv('GITHUB_REPO_URL')
 LOCAL_REPO_PATH = os.getenv('LOCAL_REPO_PATH', '/tmp/github-repo')
-INDEX_HTML_PATH = os.path.join(LOCAL_REPO_PATH, 'index.html')
-OSS_INDEX_HTML_PATH = 'index.html'  # OSS中的目标路径
 
 def clone_or_pull_repo(repo_url, local_path):
     if not os.path.exists(local_path):
@@ -38,6 +36,13 @@ def upload_file_to_oss(file_path, destination_path):
     except subprocess.CalledProcessError as e:
         print("Failed to upload file %s: %s" % (file_path, e))
 
+def upload_directory_to_oss(local_path, prefix=''):
+    for root, dirs, files in os.walk(local_path):
+        for file in files:
+            local_file_path = os.path.join(root, file)
+            oss_path = os.path.join(prefix, os.path.relpath(local_file_path, local_path)).replace("\\", "/")
+            upload_file_to_oss(local_file_path, oss_path)
+
 if __name__ == '__main__':
     # 检查必要的环境变量是否设置
     if not all([OSS_ACCESS_KEY_ID, OSS_SECRET_ACCESS_KEY, OSS_ENDPOINT, OSS_BUCKET_NAME, GITHUB_REPO_URL]):
@@ -47,8 +52,5 @@ if __name__ == '__main__':
     # 克隆或拉取GitHub仓库
     clone_or_pull_repo(GITHUB_REPO_URL, LOCAL_REPO_PATH)
 
-    # 检查index.html文件是否存在
-    if os.path.exists(INDEX_HTML_PATH):
-        upload_file_to_oss(INDEX_HTML_PATH, OSS_INDEX_HTML_PATH)
-    else:
-        print("File %s does not exist." % INDEX_HTML_PATH)
+    # 上传目录到OSS
+    upload_directory_to_oss(LOCAL_REPO_PATH)
