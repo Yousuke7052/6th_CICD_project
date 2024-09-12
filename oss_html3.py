4263454c6a8dd6f300013eab628a58f1a53b975d
littlesun <liohahaku@gmail.com>
2024-09-12 23:59:55 +0800

    update

--- oss_html3.py
+++ oss_html3.py
@@ -7,21 +7,20 @@ OSS_SECRET_ACCESS_KEY = os.getenv('OSS_SECRET_ACCESS_KEY')
 OSS_ENDPOINT = os.getenv('OSS_ENDPOINT')
 OSS_BUCKET_NAME = os.getenv('OSS_BUCKET_NAME')
 
-# 目录信息
-LOCAL_DIR_PATH = 'project_2'
-DESTINATION_PATH = 'project_2'
+# 文件信息
+LOCAL_FILE_PATH = 'index.html'
+DESTINATION_PATH = 'index.html'
 
-# 上传目录到OSS
+# 上传文件到OSS
 try:
     oss_url = 'oss://%s/%s' % (OSS_BUCKET_NAME, DESTINATION_PATH)
     subprocess.run([
-        'ossutil', 'cp', LOCAL_DIR_PATH,
+        'ossutil', 'cp', LOCAL_FILE_PATH,
         oss_url,
-        '-r',  # 递归选项
         '--access-key-id', OSS_ACCESS_KEY_ID,
         '--access-key-secret', OSS_SECRET_ACCESS_KEY,
         '--endpoint', OSS_ENDPOINT
     ], check=True)
-    print("Directory %s uploaded to OSS successfully." % LOCAL_DIR_PATH)
+    print("File %s uploaded to OSS successfully." % LOCAL_FILE_PATH)
 except subprocess.CalledProcessError as e:
-    print("Failed to upload directory %s: %s" % (LOCAL_DIR_PATH, e))
+    print("Failed to upload file %s: %s" % (LOCAL_FILE_PATH, e))
